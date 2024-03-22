cd076e7a9a570f6c2d9adedcec3e18fca9b82dc2
Saplitsyn Denis <packman17@mail.ru>
2024-03-22 18:59:12 +0300

    add request Hello World

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+import io.javalin.Javalin;
+
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return app;
+    }
+}
