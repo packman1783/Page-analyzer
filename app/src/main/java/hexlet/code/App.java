788b6472e8b38529ec054eea9389257a09e2da60
Saplitsyn Denis <packman17@mail.ru>
2024-03-23 14:38:17 +0300

    add hikari & h2 dependencies

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import io.javalin.Javalin;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Slf4j
 public class App {
     public static void main(String[] args) {
