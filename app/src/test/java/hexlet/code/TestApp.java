f9c0306cb0828664534d5668b7692d6d3d22e3e2
Saplitsyn Denis <packman17@mail.ru>
2024-04-01 16:07:47 +0300

    add simple test

---
+++ app/src/test/java/hexlet/code/TestApp.java
@@ -0,0 +1,30 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestApp {
+
+    Javalin app;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        app = App.getApp();
+    }
+
+    @Test
+    public void testMainPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/");
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("Page Analyzer");
+        });
+    }
+
+
+}
