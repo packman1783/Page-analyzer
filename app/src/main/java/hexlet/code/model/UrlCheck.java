a16683d1578f0948c8d22112b7f11f61e1e588e2
Saplitsyn Denis <packman17@mail.ru>
2024-04-04 15:14:54 +0300

    add model for url check

---
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -0,0 +1,29 @@
+package hexlet.code.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+public class UrlCheck {
+    private Long id;
+    private Integer statusCode;
+    private String title;
+    private String h1;
+    private String description;
+    private Long urlId;
+    private Timestamp createdAt;
+
+    private UrlCheck(Integer statusCode, String title, String h1, String description, Long urlId, Timestamp createdAt) {
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+        this.urlId = urlId;
+        this.createdAt = createdAt;
+    }
+}
