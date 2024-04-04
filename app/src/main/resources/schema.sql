d178b4304a8c3880e0800dd8332e80034a12a1ee
Saplitsyn Denis <packman17@mail.ru>
2024-04-04 15:16:14 +0300

    add sql table url_checks

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,21 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE urls (
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(255) NOT NULL,
-created_at TIMESTAMP NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE url_checks (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    url_id INT NOT NULL,
+
+    FOREIGN KEY (url_id) REFERENCES urls (id) ON DELETE CASCADE,
+
+    status_code INT NOT NULL,
+    title VARCHAR(255),
+    h1 VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP NOT NULL
 );
