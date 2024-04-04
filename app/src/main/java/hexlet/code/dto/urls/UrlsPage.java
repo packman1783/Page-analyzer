0d5c51981dbd04dae503c2ebfc1963f74339f73d
Saplitsyn Denis <packman17@mail.ru>
2024-04-04 20:25:52 +0300

    add map for checks for all sites

--- app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
@@ -3,12 +3,15 @@ package hexlet.code.dto.urls;
 import hexlet.code.dto.BasePage;
 import hexlet.code.model.Url;
 import java.util.List;
+import java.util.Map;
 
+import hexlet.code.model.UrlCheck;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class UrlsPage extends BasePage {
-    List<Url> urls;
+    private List<Url> urls;
+    private Map<Long, UrlCheck> checks;
 }
