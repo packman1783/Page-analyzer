df7f57041922f96e2f96bcf875a4a6160a5aac06
Saplitsyn Denis <packman17@mail.ru>
2024-04-08 18:04:00 +0300

    add check route

--- app/src/main/java/hexlet/code/util/NamedRoutes.java
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -17,7 +17,11 @@ public class NamedRoutes {
         return "/urls/" + id;
     }
 
-    public static String urlChecksPath(Long id) {return urlChecksPath(String.valueOf(id));}
+    public static String urlChecksPath(Long id) {
+        return urlChecksPath(String.valueOf(id));
+    }
 
-    public static String urlChecksPath(String id) {return "/urls/" + id + "/checks";}
+    public static String urlChecksPath(String id) {
+        return "/urls/" + id + "/checks";
+    }
 }
